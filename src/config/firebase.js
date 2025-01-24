de732f581b0ef0a4e4c688ae9dbfcf3ae297f660
Adam Noveskey <a_noveskey@yahoo.com>
2025-01-24 10:25:33 -0500

    pushing updates. added authentication, CRUD updates, and removed keys.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { localConfigs } from "../localConfigs.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Remove secret before pushing back up to repo.
 const firebase = {
-    apiKey: "AIzaSyAPTTefdwlqqABu1aLNkAhq2Ekuh61VFIM",
+    apiKey: localConfigs?.apiKey,
     authDomain: "fir-crash-course-w-react.firebaseapp.com",
     projectId: "fir-crash-course-w-react",
     storageBucket: "fir-crash-course-w-react.firebasestorage.app",
-    messagingSenderId: "428273459409",
-    appId: "1:428273459409:web:283a5aacc4e153f643e5f0",
-    measurementId: "G-DN71LMDFCQ"
+    messagingSenderId: localConfigs?.messagingSenderId,
+    appId: localConfigs?.appId,
+    measurementId: localConfigs?.measurementId
 };
 
-// Initialize Firebase
 const app = initializeApp(firebase);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
---
+++ src/main.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import Auth from "./components/auth.jsx";
+import { auth, db } from "./config/firebase.js";
+import { addDoc, collection, doc, getDocs, deleteDoc, updateDoc } from 'firebase/firestore';
+
+const App = () => {
+    const [albumList, setAlbumList] = React.useState([]);
+    const [artist, setArtist] = React.useState('');
+    const [title, setTitle] = React.useState('');
+    const [genre, setGenre] = React.useState('');
+    const [releaseDate, setReleaseDate] = React.useState(0);
+    const [updatedGenre, setUpdatedGenre] = React.useState('');
+
+    const albumsCollectionRef = collection(db, 'music-albums');
+
+    const addAlbum = async () => {
+        try {
+            await addDoc(albumsCollectionRef, {
+                artist: artist,
+                title: title,
+                genre: genre,
+                releaseDate: releaseDate,
+                userId: auth?.currentUser?.uid
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const deleteAlbum = async (id) => {
+        const albumDoc = doc(db, 'music-albums', id);
+        try {
+            await deleteDoc(albumDoc);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const updateAlbum = async (id) => {
+        const albumDoc = doc(db, 'music-albums', id);
+        try {
+            await updateDoc(albumDoc, {
+                // artist: artist,
+                // title: title,
+                genre: updatedGenre,
+                // releaseDate: releaseDate
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const getAlbumList = async () => {
+        try {
+            const data = await getDocs(albumsCollectionRef);
+            const filteredData = data.docs.map(doc => ({
+                ...doc.data(),
+                id: doc.id
+            }));
+            console.log(filteredData);
+            setAlbumList(filteredData);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getAlbumList();
+    }, []);
+
+    return (
+        <div>
+            <Auth />
+            { /* generate a form to add an album. The form should have fields for artist, title, genre, and release date in that order. The artist, title, and genre fields are string values, the releaseDate is a number value. */ }
+            <div>
+                <input type="text" placeholder="Artist" onChange={e=> setArtist(e.target.value)}/>
+                <input type="text" placeholder="Title" onChange={e=> setTitle(e.target.value)}/>
+                <input type="text" placeholder="Genre" onChange={e=> setGenre(e.target.value)}/>
+                <input type="number" placeholder="Release Date" onChange={e=> setReleaseDate(e.target.value)}/>
+                <button onClick={addAlbum}>Add Album</button>
+            </div>
+            <div>
+                <button onClick={getAlbumList}>Get Album List</button>
+                {albumList.map(album => (
+                    <div key={album.id}>
+                        <h2>{album.title}</h2>
+                        <p>{album.artist}</p>
+                        <p>{album.releaseDate}</p>
+                        <p>{album.genre}</p>
+                        <button onClick={() => deleteAlbum(album.id)}>Delete Album</button>
+                        <input type="text" placeholder="Update Genre" onChange={e=> setUpdatedGenre(e.target.value)}/>
+                        <button onClick={() => updateAlbum(album.id)}>Update Genre</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
